c561bfcd4559f5e2c8082d35024ed4aa0be2a009
jerinkurian <jrnjerin@gmail.com>
2018-11-22 21:27:29 +0100

    files added

---
+++ src/main/java/com/jenkins/sample/jenkinssample/JenkinssampleApplication.java
@@ -0,0 +1,12 @@
+package com.jenkins.sample.jenkinssample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JenkinssampleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JenkinssampleApplication.class, args);
+	}
+}
